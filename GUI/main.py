3df989284f7c69001ca9f6942b71857cd30a69ed
Hixe <hixe@hixe.fr>
2016-04-03 20:38:44 +0200

    Added AJAX/J support to load Tweets + Categories (TODO: Multi-threading)

--- GUI/main.py
+++ GUI/main.py
@@ -1,45 +1,78 @@
+#!/usr/bin/python2
 # coding: utf8
 
 from __future__ import unicode_literals
 import simplejson as json
 import random
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 app = Flask(__name__)
 
 user = "hixe33"
+max_tweets = 30
 
-cat = ['Food', 'Politics', 'Fun', 'Hats', 'Trolling', 'Video games', 'Economy']
+new_categories = []
 categories = []
+new_tweets=[]
+tweets=[]
 
-for i in range(0, len(cat)):
-    categories += [{    "name": cat[i],
-                        "color": "#%06x" % random.randint(0, 0xFFFFFF),
-                        "id": i}]
+def init():
+    global new_categories, new_tweets
+    test_cat = ['Food', 'Politics', 'Fun', 'Hats', 'Trolling', 'Video games', 'Economy']
+    new_tweets = []
+    for i in range(0, len(test_cat)):
+        new_categories += [{   "color": "#%06x" % random.randint(0, 0xFFFFFF),
+                                "id": i,
+                                "name": test_cat[i]}]
 
-
-tweets = []
-with open("tweets.json", 'r') as f:
-    t = json.loads(f.read())
-for i in range(0,20):
-    tweets += [t['tweets'][i]]
-    tweets[i]["cat"] = categories[random.randint(0, 5)]
-    tweets[i]["author"] = "Hixe"
-    tweets[i]["author_id"] = user
-    tweets[i]["id"] = "711976556613267457"
-#
-# @app.route('/')
-# def index():
-#     return render_template('index.html', categories=categories, num_cat=len(categories), tweets=tweets, username="Twitter")
+    with open("tweets.json", 'r') as f:
+        lst = json.loads(f.read())
+        for i in range(0,20):
+            new_tweets += [lst['tweets'][i]]
+            new_tweets[i]["cat"] = new_categories[random.randint(0, 6)]
+            new_tweets[i]["author"] = "Hixe"
+            new_tweets[i]["author_id"] = user
+            new_tweets[i]["id"] = "711976556613267457"
 
 @app.route('/')
 @app.route('/<username>')
-def index(username="Twitter"):
-    return render_template('index.html', categories=categories, num_cat=len(categories), tweets=tweets, username=username)
+def index(username=user):
+    return render_template('index.html', username=username)
 
 @app.route('/switch', methods=['POST', 'GET'])
 def switchUser():
-    return redirect(url_for('index', username=request.form['username']))
+    user = request.form['username']
+    return redirect(url_for('index', username=user))
+
+@app.route('/update_categories', methods=['POST', 'GET'])
+def update_categories(json=True):
+    global new_categories, categories
+
+    for c in new_categories:
+        categories += [c]
+
+    new_categories = []
+    if json:
+        return jsonify(result=categories)
+
+@app.route('/update_tweets', methods=['POST', 'GET'])
+def update_tweets(json=True):
+    global new_tweets, tweets, max_tweets, categories
+
+    for t in new_tweets:
+        tweets += [t]
+
+        if len(tweets) > max_tweets:
+            tweets.pop(0)
+
+    new_tweets = []
+    if json:
+        return jsonify(result=tweets)
+
+    # if redirect:
+    #     return redirect(url_for('index', username=user
+    # return redirect(url_for('index', username=user))
 
 if __name__ == '__main__':
+    init()
     app.run(debug=True)
--- GUI/static/js/scripts.js
+++ GUI/static/js/scripts.js
@@ -1,4 +1,6 @@
-$("form").hide();
+var reloadTime = 5000;
+
+$("#form").hide();
 
 function toggleCat(id, num)
 {
@@ -17,3 +19,76 @@ function toggleForm(name)
     $('#form-' + name).show();
     $('.display-' + name).hide();
 }
+
+function showTweets(data)
+{
+    var length = data.result.length;
+    var t;
+    $("#tweets-list").html("");
+    for(var i=length-1; i>=0; i--)
+    {
+        t = data.result[i];
+        $("#tweets-list").html($("#tweets-list").html() +
+            '<div class=" mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--12-col-desktop cat-' + t.cat.id +'">'+
+                '<div class="mdl-card__title mdl-card--expand" style="background-color: ' + t.cat.color + '">'+
+                    '<div class="head-boxes">'+
+                        '<h2 class="mdl-card__title-text">'+
+                            '<span class="user-badge">' + t.author + ' <small>(@' + t.author_id + ')</small></span>'+
+                        '</h2>'+
+                    '</div>'+
+                    '<div class="head-boxes cat-header">'+
+                        '<span class="badge">' + t.cat.name + '</span>'+
+                    '</div>'+
+
+                '</div>'+
+                '<div class="mdl-card__supporting-text mdl-color-text--grey-600">'+
+                    t.text +
+                '</div>'+
+                '<div class="mdl-card__actions mdl-card--border">'+
+                    '<a href="http://twitter.com/' + t.author_id + '/status/' + t.id + '" class="mdl-button mdl-js-button mdl-js-ripple-effect">See</a>'+
+                '</div>'+
+            '</div>')
+    }
+}
+
+function showCategories(data)
+{
+    var length = data.result.length;
+    var c;
+
+    $("#cat-list").html("");
+    for(var i=length-1; i>=0; i--)
+    {
+        c = data.result[i];
+        $("#cat-list").html($("#cat-list").html() +
+        '<a class="mdl-navigation__link cat-main-'+ c.id + ' cat-item" href="#" onclick="toggleCat('+ c.id + ', '+ length + ')">'+
+            '<i class="dot mdl-color-text--blue-grey-400 material-icons" style="background-color: '+ c.color + ';"></i>'+
+            '<span class="list-item">'+ c.name + '</span>'+
+        '</a>')
+    }
+}
+
+window.setInterval( function(){$("#refresh-tweets").click();} , reloadTime);
+window.setInterval( function(){$("#refresh-cat").click();} , reloadTime);
+
+$('a#refresh-tweets').bind('click', function(){
+    $('#refresh-tweets').hide();
+    $.getJSON('./update_tweets',
+    {json:true},
+    function(data) {
+        showTweets(data);
+    });
+
+    return false;
+});
+
+$('a#refresh-cat').bind('click', function(){
+    $('#refresh-cat').hide();
+    $.getJSON('./update_categories',
+    {json:true},
+    function(data) {
+        showCategories(data);
+    });
+
+    return false;
+});
